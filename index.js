fb09d4138ab2df92c47a4f2b959820dc8d5f29b4
MB-Liz <137509480+MB-Liz@users.noreply.github.com>
2023-08-30 23:26:34 +0400

    add destroy grid and color grid

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 const sketchpad = document.querySelector(".sketchpad-grid");
 const cell = document.querySelectorAll(".cell");
+const gridSize = document.querySelectorAll(".grid-size");
+const black = "#000";
+const white = "#fff";
 //const context = sketchpad.getContext("2d");
 
-function buildGrid(numGrid, sketchpad){
+function buildGrid(numGrid){
     sketchpad.style.setProperty('--grid-rows', numGrid);
     sketchpad.style.setProperty('--grid-cols', numGrid);
     for (let x = 1; x <= numGrid; x++){
         for(let y = 1; y <= numGrid; y++){
             let cell = document.createElement("div");
-            
+            cell.style.background = white;
             sketchpad.appendChild(cell).className = "cell";
+            cell.addEventListener('mouseover',(e) => colorCell(cell));
         }
     }
 }
 
-function colorCell(cell){
-    document.getElementsByClassName('cell')[cell].style.background = '#000'; 
-    console.log("success");
+function DestroyGrid(){
+    sketchpad.innerHTML="";  //destroy all children
+    console.log(cell.length); 
 }
 
-buildGrid(15, sketchpad);
+function colorCell(cell){
+    console.log('success');
+    cell.style.backgroundColor = black; 
+}
 
-cell.forEach(function (i){
-    i.addEventListener('mouseover', (event) => colorCell(i))
+
+console.log(cell);
+cell.forEach((item) => {
+    console.log(item);
+    i.addEventListener('click',(e) => colorCell(i));
+    console.log('time');
 });
+
+
+// Select grid size
+gridSize.forEach((size) => {
+    size.addEventListener("click", (e) =>{
+        console.log(size);
+        DestroyGrid();
+        buildGrid(size.textContent);
+    });
+    
+});
+
+
+
+// Erase
+
+buildGrid(18, sketchpad);
