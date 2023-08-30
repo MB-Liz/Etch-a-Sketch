2f4b2cf664a1f192d1d004de2d1a9d21db449710
MB-Liz <137509480+MB-Liz@users.noreply.github.com>
2023-08-31 00:23:31 +0400

    add eraseGrid, padding, style left container, rename js variable

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const sketchpad = document.querySelector(".sketchpad-grid");
 const cell = document.querySelectorAll(".cell");
-const gridSize = document.querySelectorAll(".grid-size");
+const gridSizeButton = document.querySelectorAll(".grid-size-button");
+const eraseButton = document.querySelector(".erase-button");
 const black = "#000";
 const white = "#fff";
+let defaultGridSize = 18;
+let gridSize = defaultGridSize;
+let currentColor = black;
 //const context = sketchpad.getContext("2d");
 
-function buildGrid(numGrid){
-    sketchpad.style.setProperty('--grid-rows', numGrid);
-    sketchpad.style.setProperty('--grid-cols', numGrid);
-    for (let x = 1; x <= numGrid; x++){
-        for(let y = 1; y <= numGrid; y++){
+eraseButton.addEventListener("click", eraseGrid);
+
+function buildGrid(gridSize){
+    sketchpad.style.setProperty('--grid-rows', gridSize);
+    sketchpad.style.setProperty('--grid-cols', gridSize);
+    for (let x = 1; x <= gridSize; x++){
+        for(let y = 1; y <= gridSize; y++){
             let cell = document.createElement("div");
             cell.style.background = white;
             sketchpad.appendChild(cell).className = "cell";
@@ -20,29 +26,23 @@ function buildGrid(numGrid){
 
 function DestroyGrid(){
     sketchpad.innerHTML="";  //destroy all children
-    console.log(cell.length); 
 }
 
 function colorCell(cell){
     console.log('success');
-    cell.style.backgroundColor = black; 
+    cell.style.backgroundColor = currentColor; 
 }
 
 
 console.log(cell);
-cell.forEach((item) => {
-    console.log(item);
-    i.addEventListener('click',(e) => colorCell(i));
-    console.log('time');
-});
 
 
 // Select grid size
-gridSize.forEach((size) => {
+gridSizeButton.forEach((size) => {
     size.addEventListener("click", (e) =>{
-        console.log(size);
+        gridSize = size.textContent;
         DestroyGrid();
-        buildGrid(size.textContent);
+        buildGrid(gridSize);
     });
     
 });
@@ -50,5 +50,10 @@ gridSize.forEach((size) => {
 
 
 // Erase
+function eraseGrid(){
+    DestroyGrid();
+    buildGrid(gridSize);
+}
 
-buildGrid(18, sketchpad);
+
+buildGrid(gridSize);
